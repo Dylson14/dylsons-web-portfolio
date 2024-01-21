707cff2d36454f979281cac169f3ac931589087f
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-21 15:43:32 +0000

    added the javascript to handle the menu toggle

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("everything is a ok over here boss")
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
